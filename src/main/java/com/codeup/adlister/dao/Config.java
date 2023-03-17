bd0834e023e1435c25760e23b9c888ac69434c58
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-03-17 10:13:53 -0500

    moved all files

---
+++ src/main/java/com/codeup/adlister/dao/Config.java
@@ -0,0 +1,13 @@
+package com.codeup.adlister.dao;
+
+class Config {
+    public String getUrl() {
+        return "jdbc:mysql://studentdb.fulgentcorp.com/ymir_isaac?allowPublicKeyRetrieval=true&useSSL=false";
+    }
+    public String getUser() {
+        return "ymir_isaac";
+    }
+    public String getPassword() {
+        return "5TngBepuir72KT1";
+    }
+}
